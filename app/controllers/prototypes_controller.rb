a2cf53d833a0a972f228d41b417fa807da80cf8d
hukudash1geki <fukudashgek81@gmail.com>
2023-07-03 22:29:26 +0900

    Active Storageの導入およびバリテーションの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -6,4 +6,10 @@ class PrototypesController < ApplicationController
   def new
   end
 
+  private
+
+  def message_params
+    params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
