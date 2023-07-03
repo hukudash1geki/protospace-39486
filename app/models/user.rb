08c3f2bb8ea234a078ec64a7da59e78ae1535141
hukudash1geki <fukudashgek81@gmail.com>
2023-07-03 21:18:11 +0900

    Prototypeモデルおよびテーブルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  has_many :prototypes
 
   validates :name, presence: true
   validates :email, presence: true
