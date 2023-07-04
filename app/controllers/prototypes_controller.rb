4eb1df2401c9df27bbc9433d58e898c7be6a6e32
hukudash1geki <fukudashgek81@gmail.com>
2023-07-04 15:17:03 +0900

    投稿機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,12 +4,23 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @proto = Prototype.new
+  end
+
+  def create
+    @proto = Prototype.new(proto_params)
+    @proto.save
+    if @proto.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def message_params
-    params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
+  def proto_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
 end
